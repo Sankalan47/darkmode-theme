7df5621226ce2a82d72e60cde880348f175bd709
Sankalan Dasgupta <43284744+Sankalan47@users.noreply.github.com>
2020-09-14 10:36:42 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+
+const imgs = document.getElementById("imgs");
+
+const img = document.querySelectorAll("#imgs img")
+
+let idx = 0;
+
+
+function crs() {
+    idx++;
+    if(idx > img.length-1){
+        idx = 0;
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+    setTimeout(crs, 2000);
+}
+
+crs();
